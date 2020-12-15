39be83f6c7054077fff77b09ca02ad81f1356999
bilalhassan48 <bilalhassan_bh@outlook.com>
2020-12-15 13:27:57 +0500

    get bundles

--- src/main/java/com/zingpay/zong/integration/controller/ZongBundlesController.java
+++ src/main/java/com/zingpay/zong/integration/controller/ZongBundlesController.java
@@ -1,17 +1,14 @@
 package com.zingpay.zong.integration.controller;
 
-import com.zingpay.zong.integration.dto.BundlesResponseDto;
 import com.zingpay.zong.integration.service.ZongBundlesService;
+import com.zingpay.zong.integration.util.Status;
+import com.zingpay.zong.integration.util.StatusMessage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.example.bankchannelservice.GetBundles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  * @author Bilal Hassan on 14-Dec-20
@@ -26,9 +23,9 @@ public class ZongBundlesController extends BaseController {
     @Autowired
     private ZongBundlesService zongBundlesService;
 
-    @ApiOperation(value = "Takes in ZongLoadDto as requestbody and perform zong load.", response = BundlesResponseDto.class)
-    @GetMapping
-    public List<BundlesResponseDto> getBundles(@RequestBody GetBundles bundles) {
-        return zongBundlesService.getBundlesList(bundles);
+    @ApiOperation(value = "Takes in ZongLoadDto as requestbody and perform zong load.", response = Status.class)
+    @PostMapping
+    public Status getBundles(@RequestBody GetBundles bundles) {
+        return new Status(StatusMessage.SUCCESS, zongBundlesService.getBundlesList(bundles));
     }
 }
